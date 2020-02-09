85bd96e7ba7e8df89b3a31808f09d84aba9531c0
David <david.thurnau@web.de>
2020-02-09 15:57:32 +0100

    init

---
+++ lib/dynvis_palindrome.rb
@@ -0,0 +1,6 @@
+require "dynvis_palindrome/version"
+
+module DynvisPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dynvis_palindrome/version.rb
@@ -0,0 +1,3 @@
+module DynvisPalindrome
+  VERSION = "0.1.0"
+end
